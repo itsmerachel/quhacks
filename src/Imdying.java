e6c6cdd040b53563a11fb04e2762a2de914201f9
rachel <rachelwu.47@gmail.com>
2017-12-09 12:29:45 -0500

    We have processing, boys, inside this time

---
+++ src/Imdying.java
@@ -0,0 +1,18 @@
+import processing.core.PApplet;
+
+public class Imdying extends PApplet{
+    public static void main(String[] args) {
+        PApplet.main("Imdying");
+    }
+    public void settings(){
+        size(300,300);
+    }
+
+    public void setup(){
+        fill(120,50,240);
+    }
+
+    public void draw(){
+        ellipse(width/2,height/2,second(),second());
+    }
+}
