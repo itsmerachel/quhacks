7645234af10b1e8043aefc2f2f58e58cb610dc40
rachel <rachelwu.47@gmail.com>
2017-12-09 15:18:17 -0500

    im screaming, only two hours in

--- src/yeet/test.java
+++ src/yeet/test.java
@@ -1,8 +1,41 @@
 package yeet;
 
-public class test {
+import processing.core.PApplet;
 
-    RanGen fuck = new RanGen();
+public class test extends PApplet{
+    RanGen tool = new RanGen();
+    public static void main(String[] args){
+        PApplet.main("yeet.test");
+    }
 
+    public void setup() {
+        background(0);
+        colorMode(HSB, 360, 100, 100);
 
+    }
+
+    public void settings(){
+        size(1000, 500);
+    }
+
+    public void draw(){
+        text("yes",8,8);
+        ellipse(100,100,80,95);
+    }
+
+    public void loop(){
+        text("proof", 500, 500);
+        for(int i =0;i<tool.randarray().length;i++){ //something something Cory Scaffolding
+            fill((i+1)*36, 70, 70);
+            ellipse(i*40,i*40,tool.randarray()[i],tool.randarray()[i]);
+            System.out.println(i);
+            redraw();
+        }
+
+    }
+    public void mousePressed(){
+        text("fuCKING",400,100);
+        loop();
+    }
 }
+
