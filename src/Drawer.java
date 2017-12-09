76f470c8ce9b6891a7bc43a14118d4fc1b5f324c
rachel <rachelwu.47@gmail.com>
2017-12-09 12:08:40 -0500

    We have processing, boys, inside this time

--- src/Drawer.java
+++ src/Drawer.java
@@ -1,6 +1,17 @@
 import processing.core.*;
 
-public class Drawer {
+public class Drawer extends PApplet{
+    public void settings() {
+        size(500, 500);
+    }
 
+    public void draw(){
+        background(64);
+        ellipse(mouseX, mouseY, 20, 20);
+    }
 
+    public static void main(String[] passedArgs) {
+        String[] appletArgs = new String[] { "MySketch" };
+        PApplet.main(appletArgs);
+    }
 }
