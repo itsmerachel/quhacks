9b91d526a23d841fd7152056d68c39e8c9db02c8
WuPhil <philipwu300@gmail.com>
2017-12-11 00:45:18 -0500

    H I T I T

--- src/RanGen.java
+++ src/RanGen.java
@@ -4,7 +4,10 @@ import java.util.Random;
 public class RanGen {
 
     public RanGen(){
-
+ //write code here
+        Random asd = new Random();
+        int n = asd.nextInt(100)+1;
+        System.out.println(n);
     }
 
 
