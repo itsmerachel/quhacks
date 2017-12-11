f0022fb747c13676378d068071a3102452b744d3
WuPhil <philipwu300@gmail.com>
2017-12-11 01:57:10 -0500

    quhacks

--- src/yeet/RanGen.java
+++ src/yeet/RanGen.java
@@ -8,25 +8,26 @@ public class RanGen {
     static public double fuck = 0.0;
     public int namehere[] = new int[10]; // the reason why you have to use the "new" token is because arrays are a actually their own kind of,,,,, object. the same way that an object is an object,; also uhhhharrays cant change indexes and stuff u can lokk at the documentation :) ik a decent part of this
 
-    public RanGen(){
- //write code here
+    public RanGen() {
+        //write code here
         Random asd = new Random();
-        int n = asd.nextInt(100)+1;
+        int n = asd.nextInt(100) + 1;
     }
-//push plos
+
+    //push plos
     //wait where do i put the way to put values in the arra
     // make another method; like below
-       void what(){
+    void what() {
     }
 
 
     /*constructor syntax:
-    *
-    * [public/private] [static/non static (static means that you call the method like this: Classname.method() in stead of through the object. i dont really know how to use it for optimizatiuon; but you dig.] [i forot the other adjectives you can use to describe methods] [method name] ([parameters, delimited,m by, comma){
-    *
-    * function here.
-    *
-    * }
-    * */
-    publicpppuuuuuuuuuuuuuussshshshshshshhs
+     *
+     * [public/private] [static/non static (static means that you call the method like this: Classname.method() in stead of through the object. i dont really know how to use it for optimizatiuon; but you dig.] [i forot the other adjectives you can use to describe methods] [method name] ([parameters, delimited,m by, comma){
+     *
+     * function here.
+     *
+     * }
+     * */
+
 }
